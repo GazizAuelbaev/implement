53c9a6d2f2517669f8eaa7ea4656d990d1dc1c50
GazizAuelbaev <90957946+GazizAuelbaev@users.noreply.github.com>
2022-05-13 17:50:31 +0600

    Update BST.java

--- BST.java
+++ BST.java
@@ -1,39 +1,66 @@
-import org.w3c.dom.Node;
-
-public class BST <K extends Comparable<K> ,V>{
-
+public class BST <K extends Comparable<K>, V>{
     private Node root;
 
-    private class Node{
+    private class Node {
         private K key;
-        private V value;
-        private Node left,right;
+        private V val;
+        private Node left, right;
 
-        public Node(K key, V value) {
+        public Node(K key, V val) {
             this.key = key;
-            this.value = value;
+            this.val = val;
+            left = right = null;
         }
     }
 
-    public void put(K key, V value){
-        root = put(root, key, value);
+    public BST(){
+        root = null;
     }
 
+    public BST(K key, V val){
+        root = new Node(key, val);
+    }
 
-    private Node put(Node root, K key, V value){
+    public void put(K key, V val) {
+        root = insert(root, key, val);
+    }
+
+    private Node insert(Node root, K key, V val){
         if (root == null){
-            root = new Node(key, value);
+            root = new Node(key, val);
             return root;
         }
         if (key.compareTo(root.key) < 0){
-            root.left = put(root.left, key, value);
+            root.left = insert(root.left, key, val);
         }
         else{
-            root.right = put(root.right, key, value);
+            root.right = insert(root.right, key, val);
         }
         return root;
     }
 
+    public void inOrder(){
+        inOrder(root);
+    }
+
+    public void postOrder(){
+        postOrder(root);
+    }
+
+    private void postOrder(Node root){
+        if (root != null){
+            postOrder(root.right);
+            System.out.println(root.key + " -> " + root.val);
+            postOrder(root.left);
+        }
+    }
+    private void inOrder(Node root){
+        if (root != null){
+            inOrder(root.left);
+            System.out.println(root.key + " -> " + root.val);
+            inOrder(root.right);
+        }
+    }
 
     public V get(K key){
         return get(root, key);
@@ -44,7 +71,7 @@ public class BST <K extends Comparable<K> ,V>{
             return null;
         }
         if (root.key.compareTo(key) == 0){
-            return root.value;
+            return root.val;
         }
         if (root.key.compareTo(key) > 0) return get(root.left, key);
         return get(root.right, key);
@@ -70,12 +97,33 @@ public class BST <K extends Comparable<K> ,V>{
             else if (root.right == null){
                 return root.left;
             }
+            root.key = minNode(root.right);
             root.right = delete(root.right, root.key);
         }
         return root;
     }
 
+    private K minNode(Node root){
+        K mn = root.key;
+        while(root.left != null){
+            mn = root.left.key;
+            root = root.left;
+        }
+        return mn;
+    }
+
+    public K secondMin(){
+        return secondMin(root);
+    }
+
+
+    public K secondMin(Node root){
+
+        return null;
+    }
+
     public Iterable<K> iterator(){
+
         return null;
     }
 
